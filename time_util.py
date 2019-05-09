1cbb097f17430f50bad8d685dc5e501678ca0e20
hyesun03 <chsun0303@gmail.com>
2019-05-09 22:41:50 +0900

    시간관련은 util로 뺌

---
+++ time_util.py
@@ -0,0 +1,16 @@
+import re
+from datetime import date, timedelta
+
+
+def convert_datetime(string_date: str) -> date:
+    t = list(map(int, re.findall("\d+", string_date)))
+
+    return date(t[0], t[1], t[2])
+
+
+def get_start_and_end_of_this_week():
+    current = date.today()
+    start_date = current - timedelta(days=current.weekday())
+    end_date = start_date + timedelta(days=6)
+
+    return start_date, end_date
