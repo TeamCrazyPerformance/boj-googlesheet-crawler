46ac2673ea6a8d11528978a2692fdb0257141271
hyesun03 <chsun0303@gmail.com>
2019-05-09 22:43:57 +0900

    최근 100문제 가져오게끔, 중복 문제 제거, time_util로 뺀것들 적용

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
 import requests
-import re
-from datetime import datetime
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
+from collections import OrderedDict
+
+import time_util as tu
 
 
 BASE_URL = "https://www.acmicpc.net"
@@ -10,7 +11,7 @@ BASE_URL = "https://www.acmicpc.net"
 
 def get_submission_links_from_user(boj_id: str) -> list:
     # 유저 프로필 '푼 문제'에 있는 제출 내역을 가져온다
-    # 최근 60문제의 리스트를 가져온다
+    # 최근 100문제의 리스트를 가져온다
     req = requests.get(BASE_URL + "/status?user_id=" + boj_id + "&result_id=4")
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
@@ -18,7 +19,7 @@ def get_submission_links_from_user(boj_id: str) -> list:
     data = ["/status?user_id=" + boj_id + "&result_id=4"]
     next_page = soup.select("#next_page")
 
-    for i in range(2):
+    for i in range(4):
         if len(next_page) == 0:
             break
 
@@ -49,20 +50,13 @@ def get_submissions(links: list) -> list:
             problem_id = row[2].text
             date = row[8].find('a')['title']
 
-            submit_time = convert_datetime(date)
-            start_date = datetime(2019, 5, 6, 0, 0)
-            end_date = datetime(2019, 5, 13, 0, 0)
+            submit_time = tu.convert_datetime(date)
+            start_date, end_date = tu.get_start_and_end_of_this_week()
 
-            if start_date <= submit_time < end_date:
+            if start_date <= submit_time <= end_date:
                 data.append(problem_id)
 
-    return data[::-1]   # 위(과거) -> 아래(최신)
-
-
-def convert_datetime(date: str) -> datetime:
-    t = list(map(int, re.findall("\d+", date)))
-
-    return datetime(t[0], t[1], t[2], t[3], t[4], t[5])
+    return list(OrderedDict.fromkeys(data))[::-1]   # 위(과거) -> 아래(최신)
 
 
 def get_data_from_boj() -> list:
